224ce2139d76f5bebab852c83e8c01e09fdcda1a
HakumenNC <soutartronny@gmail.com>
2020-09-23 16:56:30 +1100

    test(flutterSignalStrengthTest): Update return type

--- test/flutter_signal_strength_test.dart
+++ test/flutter_signal_strength_test.dart
@@ -18,6 +18,6 @@ void main() {
   });
 
   test('getPlatformVersion', () async {
-    expect(await FlutterSignalStrength.platformVersion, '42');
+    expect(await FlutterSignalStrength.gsmSignalStrength, 42);
   });
 }
